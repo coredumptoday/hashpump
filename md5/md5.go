0d70082163527ac8cbf4131d061c9027e1c01347
chuy.wang <chuy.wang@huolala.cn>
2021-06-18 20:15:23 +0800

    md5 hashpump

--- md5/md5.go
+++ md5/md5.go
@@ -1,6 +1,9 @@
 package md5
 
-import "fmt"
+import (
+	"encoding/binary"
+	"encoding/hex"
+)
 
 // The size of an MD5 checksum in bytes.
 const Size = 16
@@ -32,17 +35,93 @@ func (d *digest) Reset() {
 	d.len = 0
 }
 
-const (
-	magic         = "md5\x01"
-	marshaledSize = len(magic) + 4*4 + BlockSize + 8
-)
+func (d *digest) Write(p []byte) (nn int, err error) {
+	// Note that we currently call block or blockGeneric
+	// directly (guarded using haveAsm) because this allows
+	// escape analysis to see that p and d don't escape.
+	nn = len(p)
+	d.len += uint64(nn)
+	if d.nx > 0 {
+		n := copy(d.x[d.nx:], p)
+		d.nx += n
+		if d.nx == BlockSize {
+			blockGeneric(d, d.x[:])
+			d.nx = 0
+		}
+		p = p[n:]
+	}
+	if len(p) >= BlockSize {
+		n := len(p) &^ (BlockSize - 1)
+		blockGeneric(d, p[:n])
+		p = p[n:]
+	}
+	if len(p) > 0 {
+		d.nx = copy(d.x[:], p)
+	}
+	return
+}
+
+func (d *digest) checkSum() [Size]byte {
+	// Append 0x80 to the end of the message and then append zeros
+	// until the length is a multiple of 56 bytes. Finally append
+	// 8 bytes representing the message length in bits.
+	//
+	// 1 byte end marker :: 0-63 padding bytes :: 8 byte length
+	tmp := [1 + 63 + 8]byte{0x80}
+	pad := (55 - d.len) % 64                             // calculate number of padding bytes
+	binary.LittleEndian.PutUint64(tmp[1+pad:], d.len<<3) // append length in bits
+	d.Write(tmp[:1+pad+8])
+
+	// The previous write ensures that a whole number of
+	// blocks (i.e. a multiple of 64 bytes) have been hashed.
+	if d.nx != 0 {
+		panic("d.nx != 0")
+	}
+
+	var digest [Size]byte
+	binary.LittleEndian.PutUint32(digest[0:], d.s[0])
+	binary.LittleEndian.PutUint32(digest[4:], d.s[1])
+	binary.LittleEndian.PutUint32(digest[8:], d.s[2])
+	binary.LittleEndian.PutUint32(digest[12:], d.s[3])
+	return digest
+}
 
 // Sum returns the MD5 checksum of the data.
 func Sum(data []byte) [Size]byte {
 	var d digest
 	d.Reset()
-	fmt.Println(d)
-	//d.Write(data)
-	//return d.checkSum()
-	return [16]byte{}
+	d.Write(data)
+	return d.checkSum()
+}
+
+//md5延长攻击
+func HashPump(sign, origStr, extendStr string, keyLen int) (s, e []byte, err error) {
+	var d digest
+	d.Reset()
+
+	signByteArr, err := hex.DecodeString(sign)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	d.s[0] = binary.LittleEndian.Uint32(signByteArr[:4])
+	d.s[1] = binary.LittleEndian.Uint32(signByteArr[4:8])
+	d.s[2] = binary.LittleEndian.Uint32(signByteArr[8:12])
+	d.s[3] = binary.LittleEndian.Uint32(signByteArr[12:])
+
+	d.len = uint64(len(origStr) + keyLen)
+
+	tmp := [1 + 63 + 8]byte{0x80}
+	pad := (55 - d.len) % 64                             // calculate number of padding bytes
+	binary.LittleEndian.PutUint64(tmp[1+pad:], d.len<<3) // append length in bits
+
+	e = []byte(origStr)
+	e = append(e, tmp[:1+pad+8]...)
+	d.len = uint64(len(e) + keyLen)
+	e = append(e, []byte(extendStr)...)
+
+	d.Write([]byte(extendStr))
+	si := d.checkSum()
+
+	return si[:], e, nil
 }
--- test/md5_test.go
+++ test/md5_test.go
@@ -1,13 +1,44 @@
 package test
 
 import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/coredumptoday/hashpump/md5"
 )
 
-const STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const KEY = "43f55d8396d0982fd62666883a9b3730"
 
 func TestMD5(t *testing.T) {
-	md5.Sum([]byte(STR))
+	m := md5.Sum([]byte(STR))
+	fmt.Println(hex.EncodeToString(m[:]))
+}
+
+func TestMD5Pump(t *testing.T) {
+	data := "orderIds=11111,999"
+	injectData := ",22222,33333"
+
+	originMd5Str := md5.Sum([]byte(KEY + data))
+	fmt.Println("origin:", hex.EncodeToString(originMd5Str[:]), KEY+data)
+
+	newSign, newStr, err := md5.HashPump(hex.EncodeToString(originMd5Str[:]), data, injectData, len(KEY))
+	fmt.Println(hex.EncodeToString(newSign))
+	fmt.Println(string(newStr))
+	fmt.Println(err)
+
+	m := md5.Sum([]byte(KEY + string(newStr)))
+	fmt.Println(hex.EncodeToString(m[:]))
+
+	d := bytes.Split(newStr, []byte("="))
+	no := bytes.Split(d[1], []byte(","))
+
+	for _, v := range no {
+		fmt.Println(strconv.Atoi(strings.TrimSpace(string(v))))
+	}
 }
